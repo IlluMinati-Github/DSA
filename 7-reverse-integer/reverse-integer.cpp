ca42ba1c831d37adcf75e27dccc82ae7a158daf6
Harsh Gupta <141059396+IlluMinati-Github@users.noreply.github.com>
2025-07-03 13:02:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (82.27%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -4,7 +4,10 @@ public:
         int ans=0,i=0;
         while(x!=0){
             int digit=x%10;
-            if ((ans>(INT_MAX/10)) || (ans<(INT_MIN/10))){
+            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && digit > 7)) {
+                return 0;
+            }
+            if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && digit < -8)) {
                 return 0;
             }
             ans=digit+(ans*10);
