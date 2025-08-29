74d97e90beed0fb2f5742163a290f572ac2a6c2a
Harsh Gupta <141059396+IlluMinati-Github@users.noreply.github.com>
2025-08-29 23:08:58 +0530

    Time: 3 ms (18.89%) | Memory: 7.8 MB (74.86%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int getNext(int n) {
+        int total = 0;
+        while (n > 0) {
+            int d = n % 10;
+            total += pow(d,2);
+            n /= 10;
+        }
+        return total;
+    }
+
+    bool isHappy(int n) {
+        for (int i = 0; i < 1000; i++) {  
+            if (n == 1) return true;
+            n = getNext(n);
+        }
+        return false;
+    }
+};
