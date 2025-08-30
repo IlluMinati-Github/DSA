39bc5b40f58fb361323abbc9121d037ae3b85a55
Harsh Gupta <141059396+IlluMinati-Github@users.noreply.github.com>
2025-08-31 02:17:33 +0530

    Time: 4 ms (64.67%) | Memory: 13.4 MB (53.33%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> result,x,y;
-        for(int i=0;i<n;i++){
-            x.push_back(nums[i]);
-        }
-        for(int i=n;i<nums.size();i++){
-            y.push_back(nums[i]);
-        }
-        int i=0,j=0;
-        while(i<n){
-            result.push_back(x[i]);
-            i++;
-            result.push_back(y[j]);
-            j++;
+        vector<int> result;
+        for (int i = 0; i < n; i++) {
+            result.push_back(nums[i]);      // x_i
+            result.push_back(nums[i + n]);  // y_i
         }
         return result;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> shuffle(vector<int>& nums, int n) {
+//         vector<int> result,x,y;
+//         for(int i=0;i<n;i++){
+//             x.push_back(nums[i]);
+//         }
+//         for(int i=n;i<nums.size();i++){
+//             y.push_back(nums[i]);
+//         }
+//         int i=0,j=0;
+//         while(i<n){
+//             result.push_back(x[i]);
+//             i++;
+//             result.push_back(y[j]);
+//             j++;
+//         }
+//         return result;
+//     }
+// };
