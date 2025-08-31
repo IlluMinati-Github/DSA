928159de2833ac3206854043fa74605a52cddb2e
Harsh Gupta <141059396+IlluMinati-Github@users.noreply.github.com>
2025-08-31 13:01:11 +0530

    Time: 3 ms (24.58%) | Memory: 28.4 MB (21.04%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -1,33 +1,53 @@
 class Solution {
 public:
-    int minimum(vector<int>& nums){
-        int minVal = INT_MAX;
-        int id = -1;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] < minVal){
-                minVal = nums[i];
-                id = i;
-            }
-        }
-        return id;
-    }
-
     vector<int> numberGame(vector<int>& nums) {
         vector<int> arr;
+        sort(nums.begin(), nums.end()); 
 
-        while(!nums.empty()){
-            int d = minimum(nums);
-            int a = nums[d];
-            nums.erase(nums.begin() + d);
+        for(int i = 0; i < nums.size(); i += 2){
+            int alice = nums[i];       
+            int bob = nums[i+1];       
 
-            int c = minimum(nums);
-            int b = nums[c];
-            nums.erase(nums.begin() + c);
-
-            arr.push_back(b);
-            arr.push_back(a);
+            arr.push_back(bob);        
+            arr.push_back(alice);      
         }
 
         return arr;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int minimum(vector<int>& nums){
+//         int minVal = INT_MAX;
+//         int id = -1;
+//         for(int i = 0; i < nums.size(); i++){
+//             if(nums[i] < minVal){
+//                 minVal = nums[i];
+//                 id = i;
+//             }
+//         }
+//         return id;
+//     }
+
+//     vector<int> numberGame(vector<int>& nums) {
+//         vector<int> arr;
+
+//         while(!nums.empty()){
+//             int d = minimum(nums);
+//             int a = nums[d];
+//             nums.erase(nums.begin() + d);
+
+//             int c = minimum(nums);
+//             int b = nums[c];
+//             nums.erase(nums.begin() + c);
+
+//             arr.push_back(b);
+//             arr.push_back(a);
+//         }
+
+//         return arr;
+//     }
+// };
