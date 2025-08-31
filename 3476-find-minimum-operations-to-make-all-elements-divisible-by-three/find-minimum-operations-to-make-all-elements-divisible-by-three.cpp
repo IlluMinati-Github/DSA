f31fb63efada10d679cf59f05f6ee7e1b535177d
Harsh Gupta <141059396+IlluMinati-Github@users.noreply.github.com>
2025-08-31 13:51:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (12.21%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -3,30 +3,41 @@ public:
     int minimumOperations(vector<int>& nums) {
         int count=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]%3!=0){
-                int a=nums[i];
-                int b=nums[i];
-                int plus_count=0;
-                int minus_count=0;
-                for(int j=0;j<3;j++){
-                    a--;  
-                    minus_count++;                  
-                    if(a%3==0){
-                        break;
-                    }
+            count += min(nums[i] % 3, 3 - nums[i] % 3);
 
-                }
-                for(int j=0;j<3;j++){
-                    b++;  
-                    plus_count++;                                      
-                    if(b%3==0){
-                        break;
-                    }
-                }
-                int c=min(plus_count,minus_count);
-                count+=c;
-            }
         }
         return count;
     }
 };
+// class Solution {
+// public:
+//     int minimumOperations(vector<int>& nums) {
+//         int count=0;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]%3!=0){
+//                 int a=nums[i];
+//                 int b=nums[i];
+//                 int plus_count=0;
+//                 int minus_count=0;
+//                 for(int j=0;j<3;j++){
+//                     a--;  
+//                     minus_count++;                  
+//                     if(a%3==0){
+//                         break;
+//                     }
+
+//                 }
+//                 for(int j=0;j<3;j++){
+//                     b++;  
+//                     plus_count++;                                      
+//                     if(b%3==0){
+//                         break;
+//                     }
+//                 }
+//                 int c=min(plus_count,minus_count);
+//                 count+=c;
+//             }
+//         }
+//         return count;
+//     }
+// };
