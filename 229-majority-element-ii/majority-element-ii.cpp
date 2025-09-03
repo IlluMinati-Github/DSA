f3bb460b032d3444fa38f3b0ffb60df013b043be
Harsh Gupta <141059396+IlluMinati-Github@users.noreply.github.com>
2025-09-03 16:07:04 +0530

    Time: 4 ms (49.41%) | Memory: 23.6 MB (62.13%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -3,12 +3,13 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> result;
-        if (nums.empty()) return result;
+        if (nums.empty())
+            return result;
 
-        sort(nums.begin(), nums.end()); // sort the array
+        sort(nums.begin(), nums.end());
 
         int n = nums.size();
-        int count = 1; // count current number
+        int count = 1;
 
         for (int i = 1; i < n; i++) {
             if (nums[i] == nums[i - 1]) {
@@ -17,11 +18,10 @@ public:
                 if (count > n / 3) {
                     result.push_back(nums[i - 1]);
                 }
-                count = 1; // reset count for new number
+                count = 1;
             }
         }
 
-        // check the last number
         if (count > n / 3) {
             result.push_back(nums[n - 1]);
         }
@@ -29,3 +29,28 @@ public:
         return result;
     }
 };
+
+// TimeLimit Exceed
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//        vector<int> result;
+//        for(int i=0;i<nums.size();i++){
+//         int count=0;
+//         bool counted=false;
+//         for(int k=0;k<i;k++){
+//             if(nums[i]==nums[k]){
+//                 counted=true;
+//             }
+//         }
+//         if(counted) continue;
+//         for(int j=0;j<nums.size();j++){
+//             if(nums[i]==nums[j]) count++;
+//         }
+//         if(count>(nums.size()/3)){
+//             result.push_back(nums[i]);
+//         }
+//        }
+//        return result;
+//     }
+// };
