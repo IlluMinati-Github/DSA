8f6e2edb3fc16c1bfc97e8074d26e121c1d81af5
Harsh Gupta <141059396+IlluMinati-Github@users.noreply.github.com>
2025-09-03 22:16:53 +0530

    Time: 1 ms (56.06%) | Memory: 9.2 MB (87.13%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string word="";
+        for(int i=0;i<s.size();i++){
+            if (s[i]!='i'){
+                word+=s[i];
+            }
+            else {
+                reverse(word.begin(),word.end());
+            }
+        }
+        return word;
+    }
+};
