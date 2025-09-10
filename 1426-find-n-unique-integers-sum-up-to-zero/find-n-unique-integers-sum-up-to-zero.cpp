98d2d9bc383910cadc2a731dfdfe885aef498943
Harsh Gupta <141059396+IlluMinati-Github@users.noreply.github.com>
2025-09-10 13:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (55.24%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -3,16 +3,11 @@ public:
     vector<int> sumZero(int n) {
         vector<int> result;
         int a = n / 2;
-        if (n % 2 == 0) {
-            for (int i = -a; i < a + 1; i++) {
-                if (i == 0)
-                    continue;
+        for (int i = -a; i < a + 1; i++) {
+            if (i == 0 && n % 2 == 0)
+                continue;
+            else
                 result.push_back(i);
-            }
-        } else {
-            for (int i = -a; i < a + 1; i++) {
-                result.push_back(i);
-            }
         }
         return result;
     }
