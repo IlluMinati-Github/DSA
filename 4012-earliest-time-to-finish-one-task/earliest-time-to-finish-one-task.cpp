4387a2f7e3ac906adbdffff24f47a851ffb90668
Harsh Gupta <141059396+IlluMinati-Github@users.noreply.github.com>
2025-09-16 17:43:19 +0530

    Time: 3 ms (27.14%) | Memory: 36.8 MB (78.85%) - LeetSync

---
+++ 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int earliestTime(vector<vector<int>>& tasks) {
+        int n=tasks.size();
+        int ans=INT_MAX;
+        for(int i=0;i<n;i++){
+            int k=tasks[i][0]+tasks[i][1];
+            ans=min(ans,k);
+        }
+        return ans;
+    }
+};
