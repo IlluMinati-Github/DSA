720c87eb60aecc76d7c7faf02fca217dfa6eb757
Harsh Gupta <141059396+IlluMinati-Github@users.noreply.github.com>
2025-09-17 16:56:04 +0530

    Time: 2 ms (87.72%) | Memory: 85.6 MB (83.53%) - LeetSync

---
+++ 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int smallestAbsent(vector<int>& nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        }
+
+        double realAvg = (double)sum / nums.size();
+        int avg = floor(realAvg) + 1;   // smallest integer > average
+        if (avg <= 0) avg = 1;          // must be positive
+
+        while (true) {
+            bool found = false;
+            for (int i = 0; i < nums.size(); i++) {
+                if (nums[i] == avg) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return avg; 
+            }
+            avg++;
+        }
+    }
+};
