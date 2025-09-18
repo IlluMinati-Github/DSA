1aa97764c269c6a5d0d7465e4880c2885daf6cf5
Harsh Gupta <141059396+IlluMinati-Github@users.noreply.github.com>
2025-09-18 17:00:23 +0530

    Time: 18 ms (5.04%) | Memory: 14.1 MB (94.12%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -6,10 +6,8 @@ public:
         for (int i=0;i<l;i++){
             int count=0;
             for (int j=0;j<l;j++){
-                if (i!=j){
-                    if (nums[i]>nums[j]){
+                if (i!=j && nums[i]>nums[j]){
                         count++;
-                    }
                 }
             }
             arr[i]=count;
