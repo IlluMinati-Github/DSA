e267ca613ff8d36054a2d4b5dcb62b9bd7d91095
Harsh Gupta <141059396+IlluMinati-Github@users.noreply.github.com>
2026-04-17 21:46:54 +0530

    Time: 3 ms (89.37%) | Memory: 46.7 MB (50.05%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,43 +1,24 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        
-        Arrays.sort(nums); 
-        
+        Arrays.sort(nums);
         boolean[] used = new boolean[nums.length];
-        
-        num(nums, used, new ArrayList<>(), result);
+        calculate(nums,used,new ArrayList<>(),result);
         return result;
     }
-
-    public void num(int[] nums, boolean[] used, List<Integer> temp, List<List<Integer>> result) {
-
-        // base case
-        if (temp.size() == nums.length) {
+    public void calculate(int[] nums,boolean[] used,List<Integer> temp,List<List<Integer>> result){
+        if(temp.size()==nums.length){
             result.add(new ArrayList<>(temp));
             return;
         }
-
-        for (int i = 0; i < nums.length; i++) {
-
-            // already used → skip
-            if (used[i]) continue;
-
-            //  duplicate condition (MOST IMPORTANT)
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
-
-            // choose
-            used[i] = true;
+        for(int i=0;i<nums.length;i++){
+            if(used[i]) continue;
+            if(i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
+            used[i]=true;
             temp.add(nums[i]);
-
-            // recurse
-            num(nums, used, temp, result);
-
-            // backtrack
-            temp.remove(temp.size() - 1);
-            used[i] = false;
+            calculate(nums,used,temp,result);
+            temp.remove(temp.size()-1);
+            used[i]=false;
         }
     }
 }
